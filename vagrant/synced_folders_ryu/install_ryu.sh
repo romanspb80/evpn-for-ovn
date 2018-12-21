39948d9def4086badcbdbc96a5c8a7adcfb55cdb
root@10.0.3.3 <root@10.0.3.3>
2018-12-21 18:59:13 +0300

    Modify Vagrant

--- vagrant/synced_folders_ryu/install_ryu.sh
+++ vagrant/synced_folders_ryu/install_ryu.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y git
-
-sudo apt-get install mininet
-
-sudo apt-get install git python-dev python-setuptools python-pip
+sudo apt-get install python-dev python-setuptools python-pip
 git clone https://github.com/osrg/ryu.git
 cd ryu
 pip install .
