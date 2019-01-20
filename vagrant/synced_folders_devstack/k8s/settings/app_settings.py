fb7b69f95eceaff68911a863a81c6ddf2698b585
roman <romanspb@mail.ru>
2019-01-20 14:06:31 +0300

    Change RYU-application name

--- vagrant/synced_folders_devstack/k8s/settings/app_settings.py
+++ vagrant/synced_folders_devstack/k8s/settings/app_settings.py
@@ -1,5 +1,5 @@
 # Settings for RabbitMQ Server connection (DEVSTACK)
-RABBITMQ_SERVER='192.168.123.140'
+RABBITMQ_SERVER='192.168.10.100'
 RABBIT_USER='stackrabbit'
 RABBIT_PASSWORD='password'
 
@@ -10,9 +10,9 @@ PASSWORD='password'
 PORT_SSH=22
 
 # Network Node IP address (OVS bridge)
-DATAPATH_ADDR='192.168.123.140'
+DATAPATH_ADDR='192.168.10.100'
 # Neutron Server (OVN Central) IP address
-OVNCENTR_ADDR='192.168.123.140'
+OVNCENTR_ADDR='192.168.10.100'
 
 # IP address of the External system
-RYU_ADDR='192.168.123.120'
+RYU_ADDR='192.168.10.10'
