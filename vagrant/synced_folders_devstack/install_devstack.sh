8f9ec32b04f7387e576fb283833ac0c39296dacc
Roman Ptitcyn <rptitcyn@brain4net.com>
2019-02-28 15:38:02 +0300

    Add ssh

--- vagrant/synced_folders_devstack/install_devstack.sh
+++ vagrant/synced_folders_devstack/install_devstack.sh
@@ -8,6 +8,6 @@ git clone https://git.openstack.org/openstack-dev/devstack.git
 ./devstack/tools/create-stack-user.sh
 sudo su -l stack -c "git clone https://git.openstack.org/openstack-dev/devstack.git"
 sudo su -l stack -c "git clone https://git.openstack.org/openstack/networking-ovn.git"
-sudo su -l stack -c "cd devstack && git checkout 698796f1aeb0d9a559488bad9f1d03e4941b061e && cd .."
+#sudo su -l stack -c "cd devstack && git checkout 698796f1aeb0d9a559488bad9f1d03e4941b061e && cd .."
 sudo su -l stack -c "cp ./networking-ovn/devstack/local.conf.sample ./devstack/local.conf"
 sudo su -l stack -c "./devstack/stack.sh"
