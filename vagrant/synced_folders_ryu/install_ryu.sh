af8762025daa0327601f8b9a462441b0f7edcb6e
Roman Ptitcyn <rptitcyn@brain4net.com>
2019-03-05 19:07:07 +0300

    iptables config for communicate with bgp-speaker on k8s-cluster

--- vagrant/synced_folders_ryu/install_ryu.sh
+++ vagrant/synced_folders_ryu/install_ryu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y python-dev python-setuptools python-pip
+sudo apt-get install -y python-dev python-setuptools python-pip iptables
 git clone https://github.com/osrg/ryu.git
 cd ryu
 pip install .
+sudo su && iptables -t nat -A PREROUTING -p TCP --dport 179 -j REDIRECT --to-port 30179
