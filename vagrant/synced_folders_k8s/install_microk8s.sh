fb0b993651c9819aca9920bcb299737f0424729f
Roman Ptitcyn <romanspb@mail.ru>
2020-01-05 22:37:49 +0300

    Fix the path of ConfigMap

--- vagrant/synced_folders_k8s/install_microk8s.sh
+++ vagrant/synced_folders_k8s/install_microk8s.sh
@@ -7,7 +7,7 @@ sudo snap enable microk8s
 sudo sleep 1m
 cd ./k8s
 sudo microk8s.enable dns ingress
-sudo microk8s.kubectl create configmap app-settings --from-file=settings/app_settings.py
+sudo microk8s.kubectl create configmap app-settings --from-file=k8s/settings/app_settings.py
 sudo microk8s.kubectl create -f evpn-api-ing.yaml
 sudo microk8s.kubectl create -f evpn-api-rs.yaml
 sudo microk8s.kubectl create -f evpn-api-svc.yaml
