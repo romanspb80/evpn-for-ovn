151386a046701fd4dd5265172b6930a7e473c4a3
Roman Ptitcyn <romanspb@yahoo.com>
2020-05-31 14:41:36 +0300

    Add Playbook

--- vagrant/synced_folders_devstack/install_devstack.sh
+++ vagrant/synced_folders_devstack/install_devstack.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y git
-sudo apt-get install -y crudini
-
-git clone https://git.openstack.org/openstack-dev/devstack.git
-./devstack/tools/create-stack-user.sh
-sudo su -l stack -c "git clone https://git.openstack.org/openstack-dev/devstack.git"
-sudo su -l stack -c "git clone https://git.openstack.org/openstack/networking-ovn.git"
-#sudo su -l stack -c "cd devstack && git checkout 698796f1aeb0d9a559488bad9f1d03e4941b061e && cd .."
-sudo su -l stack -c "cp ./networking-ovn/devstack/local.conf.sample ./devstack/local.conf"
-sudo su -l stack -c "./devstack/stack.sh"
+su -l stack -c "git clone https://git.openstack.org/openstack-dev/devstack.git"
+su -l stack -c "git clone https://git.openstack.org/openstack/networking-ovn.git"
+#su -l stack -c "cd devstack && git checkout 698796f1aeb0d9a559488bad9f1d03e4941b061e && cd .."
+su -l stack -c "cp ./networking-ovn/devstack/local.conf.sample ./devstack/local.conf"
+su -l stack -c "./devstack/stack.sh"
--- vagrant/synced_folders_k8s/install_microk8s.sh
+++ vagrant/synced_folders_k8s/install_microk8s.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install -y snapd
-sudo snap install microk8s --classic --channel=1.13/stable
-sudo snap disable microk8s
-sudo snap enable microk8s
-sudo sleep 1m
-cd ./k8s
-sudo microk8s.enable dns ingress
-sudo microk8s.kubectl create configmap app-settings --from-file=settings/app_settings.py
-sudo microk8s.kubectl create -f evpn-api-ing.yaml
-sudo microk8s.kubectl create -f evpn-api-rs.yaml
-sudo microk8s.kubectl create -f evpn-api-svc.yaml
-#sudo microk8s.kubectl create -f evpn-agent-rs_debug.yaml
+microk8s.enable dns ingress
+microk8s.kubectl create configmap app-settings --from-file=settings/app_settings.py
+microk8s.kubectl create -f evpn-api-ing.yaml
+microk8s.kubectl create -f evpn-api-rs.yaml
+microk8s.kubectl create -f evpn-api-svc.yaml
+# microk8s.kubectl create -f evpn-agent-rs_debug.yaml
 #sudo microk8s.kubectl create -f evpn-agent-svc.yaml
-sudo microk8s.kubectl create -f evpn-agent-ds.yaml
-sudo microk8s.kubectl create -f rabbitmq-svc.yaml
-sudo microk8s.kubectl create -f rabbitmq-endpnt.yaml
-#sudo microk8s.kubectl create -f bgp-svc.yaml
-#sudo microk8s.kubectl create -f bgp-endpnt.yaml
-#sudo microk8s.kubectl create -f devstack-ssh-svc.yaml
-#sudo microk8s.kubectl create -f devstack-ssh-endpnt.yaml
+microk8s.kubectl create -f evpn-agent-ds.yaml
+microk8s.kubectl create -f rabbitmq-svc.yaml
+microk8s.kubectl create -f rabbitmq-endpnt.yaml
+# microk8s.kubectl create -f bgp-svc.yaml
+# microk8s.kubectl create -f bgp-endpnt.yaml
+# microk8s.kubectl create -f devstack-ssh-svc.yaml
+# microk8s.kubectl create -f devstack-ssh-endpnt.yaml
