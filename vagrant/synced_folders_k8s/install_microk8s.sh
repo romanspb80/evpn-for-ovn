2e7898ed621e51beac0272797e46843f8b3bdaa0
Roman Ptitcyn <romanspb@yahoo.com>
2022-01-05 23:34:24 +0300

    Rework the project

--- vagrant/synced_folders_k8s/install_microk8s.sh
+++ vagrant/synced_folders_k8s/install_microk8s.sh
@@ -5,12 +5,11 @@ microk8s.kubectl create configmap app-settings --from-file=settings/app_settings
 microk8s.kubectl create -f evpn-api-ing.yaml
 microk8s.kubectl create -f evpn-api-rs.yaml
 microk8s.kubectl create -f evpn-api-svc.yaml
-# microk8s.kubectl create -f evpn-agent-rs_debug.yaml
-#sudo microk8s.kubectl create -f evpn-agent-svc.yaml
+microk8s.kubectl create -f evpn-agent-svc.yaml
 microk8s.kubectl create -f evpn-agent-ds.yaml
 microk8s.kubectl create -f rabbitmq-svc.yaml
 microk8s.kubectl create -f rabbitmq-endpnt.yaml
-# microk8s.kubectl create -f bgp-svc.yaml
-# microk8s.kubectl create -f bgp-endpnt.yaml
-# microk8s.kubectl create -f devstack-ssh-svc.yaml
-# microk8s.kubectl create -f devstack-ssh-endpnt.yaml
+microk8s.kubectl create -f bgp-svc.yaml
+microk8s.kubectl create -f bgp-endpnt.yaml
+microk8s.kubectl create -f ovsdb-svc.yaml
+microk8s.kubectl create -f ovsdb-endpnt.yaml
