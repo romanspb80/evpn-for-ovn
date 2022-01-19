8ce1ca40be344853acfc29edd055a20b34fc6f83
Roman Ptitcyn <romanspb@yahoo.com>
2022-01-20 00:39:53 +0300

    Add helm chart

--- vagrant/synced_folders_k8s/install_microk8s.sh
+++ vagrant/synced_folders_k8s/install_microk8s.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-microk8s.enable dns ingress
-microk8s.kubectl create configmap app-settings --from-file=settings/app_settings.py
-microk8s.kubectl create -f evpn-api-ing.yaml
-microk8s.kubectl create -f evpn-api-rs.yaml
-microk8s.kubectl create -f evpn-api-svc.yaml
-microk8s.kubectl create -f evpn-agent-svc.yaml
-microk8s.kubectl create -f evpn-agent-ds.yaml
-microk8s.kubectl create -f rabbitmq-svc.yaml
-microk8s.kubectl create -f rabbitmq-endpnt.yaml
-microk8s.kubectl create -f bgp-svc.yaml
-microk8s.kubectl create -f bgp-endpnt.yaml
-microk8s.kubectl create -f ovsdb-svc.yaml
-microk8s.kubectl create -f ovsdb-endpnt.yaml
+kubectl create configmap app-settings --from-file=settings/app_settings.py
+kubectl create -f evpn-api-ing.yaml
+kubectl create -f evpn-api-rs.yaml
+kubectl create -f evpn-api-svc.yaml
+kubectl create -f evpn-agent-svc.yaml
+kubectl create -f evpn-agent-ds.yaml
+kubectl create -f rabbitmq-svc.yaml
+kubectl create -f rabbitmq-endpnt.yaml
+kubectl create -f bgp-svc.yaml
+kubectl create -f bgp-endpnt.yaml
+kubectl create -f ovsdb-svc.yaml
+kubectl create -f ovsdb-endpnt.yaml
